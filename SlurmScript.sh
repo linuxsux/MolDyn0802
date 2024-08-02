43dd5e6213f4e6be73c3309fba3768b4343464b5
linuxsux <146971492+linuxsux@users.noreply.github.com>
2024-08-02 15:13:13 -0400

    Add files via upload

---
+++ SlurmScript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=4000M
+#SBATCH --time=0-01:00:00
+# Usage: sbatch $0
+
+ 
+module purge
+module load StdEnv/2020 gcc/9.3.0 cuda/11.4 openmpi/4.0.3
+module load python/3.8.10 openmm/7.7.0 netcdf/4.7.4 hdf5/1.10.6 mpi4py/3.0.3
+source $HOME/env-parmed/bin/activate
+
+python Test2.py
